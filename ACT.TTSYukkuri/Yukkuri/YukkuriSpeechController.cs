6b068162e4d1e91e18ffeb603d93f7b4ee6298d3
anoyetta <anoyetta@gmail.com>
2014-11-01 17:55:54 +0900

    ゆっくりで漢字, 数字, アルファベットを読めるようにした

--- ACT.TTSYukkuri/Yukkuri/YukkuriSpeechController.cs
+++ ACT.TTSYukkuri/Yukkuri/YukkuriSpeechController.cs
@@ -3,7 +3,10 @@
     using System;
     using System.IO;
     using System.Runtime.InteropServices;
+    using System.Text;
+    using System.Text.RegularExpressions;
     using System.Windows.Forms;
+    using Microsoft.VisualBasic;
 
     /// <summary>
     /// ゆっくりスピーチコントローラ
@@ -64,14 +67,23 @@
 
             try
             {
+                if (string.IsNullOrWhiteSpace(text))
+                {
+                    return;
+                }
+
+                // よみがなに変換する
+                var textByYomigana = this.ConvertYomigana(text);
+
                 // テキストを音声データに変換する
                 uint size = 0;
                 wavePtr = AquesTalk_Synthe(
-                    text,
+                    textByYomigana,
                     SpeechSpeed,
                     ref size);
 
-                if (wavePtr == IntPtr.Zero)
+                if (wavePtr == IntPtr.Zero ||
+                    size <= 0)
                 {
                     return;
                 }
@@ -94,5 +106,118 @@
                 }
             }
         }
+
+        /// <summary>
+        /// よみがなに変換する
+        /// </summary>
+        /// <param name="textToConvert">変換するテキスト</param>
+        /// <returns>よみがなに変換したテキスト</returns>
+        private string ConvertYomigana(
+            string textToConvert)
+        {
+            var yomigana = textToConvert;
+
+            var ifelang = Activator.CreateInstance(Type.GetTypeFromProgID("MSIME.Japan")) as IFELanguage;
+
+            var hr = ifelang.Open();
+            if (hr != 0)
+            {
+                return yomigana;
+            }
+
+            string t;
+            hr = ifelang.GetPhonetic(textToConvert, 1, -1, out t);
+            if (hr != 0)
+            {
+                return yomigana;
+            }
+
+            yomigana = t;
+
+            // スペースを置き換える
+            yomigana = yomigana.Replace(" ", "、");
+            yomigana = yomigana.Replace("　", "、");
+
+            // 感嘆符を置き換える
+            yomigana = yomigana.Replace("!", "。");
+            yomigana = yomigana.Replace("！", "。");
+
+            // アルファベットを置き換える
+            var regex2 = new Regex(@"[a-zA-Zａ-ｚＡ-Ｚ]+");
+            yomigana = regex2.Replace(
+                yomigana,
+                (match) =>
+                {
+                    var replacement = match.Value;
+                    replacement = Strings.StrConv(replacement, VbStrConv.Narrow);
+                    replacement = Strings.StrConv(replacement, VbStrConv.Uppercase);
+                    return replacement;
+                });
+
+            yomigana = yomigana.Replace("A", "あるふぁ");
+            yomigana = yomigana.Replace("B", "ぶらぼー");
+            yomigana = yomigana.Replace("C", "ちゃーりー");
+            yomigana = yomigana.Replace("D", "でるた");
+            yomigana = yomigana.Replace("E", "えこー");
+            yomigana = yomigana.Replace("F", "ふぉっくす");
+            yomigana = yomigana.Replace("G", "ごるふ");
+            yomigana = yomigana.Replace("H", "ほてる");
+            yomigana = yomigana.Replace("I", "いんど");
+            yomigana = yomigana.Replace("J", "じゅりえっと");
+            yomigana = yomigana.Replace("K", "きろ");
+            yomigana = yomigana.Replace("L", "りま");
+            yomigana = yomigana.Replace("M", "まいく");
+            yomigana = yomigana.Replace("N", "のーべんばー");
+            yomigana = yomigana.Replace("O", "おすかー");
+            yomigana = yomigana.Replace("P", "ぱぱ");
+            yomigana = yomigana.Replace("Q", "きゅーべっく");
+            yomigana = yomigana.Replace("R", "ろめお");
+            yomigana = yomigana.Replace("S", "しえら");
+            yomigana = yomigana.Replace("T", "たんご");
+            yomigana = yomigana.Replace("U", "ゆにふぉーむ");
+            yomigana = yomigana.Replace("V", "びくたー");
+            yomigana = yomigana.Replace("W", "ういすきー");
+            yomigana = yomigana.Replace("X", "えっくすれい");
+            yomigana = yomigana.Replace("Y", "やんきー");
+            yomigana = yomigana.Replace("Z", "ずーるー");
+
+            // 数字を音声記号に置き換える
+            yomigana = yomigana.Replace("０", "0");
+            yomigana = yomigana.Replace("１", "1");
+            yomigana = yomigana.Replace("２", "2");
+            yomigana = yomigana.Replace("３", "3");
+            yomigana = yomigana.Replace("４", "4");
+            yomigana = yomigana.Replace("５", "5");
+            yomigana = yomigana.Replace("６", "6");
+            yomigana = yomigana.Replace("７", "7");
+            yomigana = yomigana.Replace("８", "8");
+            yomigana = yomigana.Replace("９", "9");
+
+            var regex1 = new Regex(@"\d+");
+            yomigana = regex1.Replace(
+                yomigana,
+                (match) =>
+                {
+                    return "<NUMK VAL=" + match.Value + ">";
+                });
+
+            return yomigana;
+        }
+    }
+
+    /// <summary>
+    /// IFELanguage Interface
+    /// </summary>
+    [ComImport]
+    [Guid("019F7152-E6DB-11d0-83C3-00C04FDDB82E")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IFELanguage
+    {
+        int Open();
+        int Close();
+        int GetJMorphResult(uint dwRequest, uint dwCMode, int cwchInput, [MarshalAs(UnmanagedType.LPWStr)] string pwchInput, IntPtr pfCInfo, out object ppResult);
+        int GetConversionModeCaps(ref uint pdwCaps);
+        int GetPhonetic([MarshalAs(UnmanagedType.BStr)] string @string, int start, int length, [MarshalAs(UnmanagedType.BStr)] out string result);
+        int GetConversion([MarshalAs(UnmanagedType.BStr)] string @string, int start, int length, [MarshalAs(UnmanagedType.BStr)] out string result);
     }
 }
