159fd94cdf034647955cd4bae0ab05b845e0ef4e
anoyetta <anoyetta@gmail.com>
2014-11-07 12:29:32 +0900

    言語とカルチャーを指定した

--- ACT.TTSYukkuri/Properties/AssemblyInfo.cs
+++ ACT.TTSYukkuri/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 [assembly: AssemblyTitle("ACT.TTSYukkuri")]
 [assembly: AssemblyDescription("https://github.com/anoyetta/ACT.TTSYukkuri")]
@@ -8,7 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ACT.TTSYukkuri")]
 [assembly: AssemblyCopyright("Copyright (c) anoyetta 2014")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("ja-JP")]
+[assembly: NeutralResourcesLanguageAttribute("ja-JP")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("fc5518a8-181b-43b2-bc12-298484b2919c")]
