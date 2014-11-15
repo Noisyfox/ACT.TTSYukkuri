508f49f50dc139339e563f31aac48a6f9d989f63
anoyetta <anoyetta@gmail.com>
2014-11-15 10:29:46 +0900

    TTSでの例外を捕捉するようにした（TTSYukkuri内の例外をACT本体に伝えたくないため）

--- ACT.TTSYukkuri/TTSYukkuriPlugin.FormActMain_newTTS.cs
+++ ACT.TTSYukkuri/TTSYukkuriPlugin.FormActMain_newTTS.cs
@@ -1,5 +1,7 @@
 ﻿namespace ACT.TTSYukkuri
 {
+    using System;
+
     using Advanced_Combat_Tracker;
 
     /// <summary>
@@ -29,40 +31,49 @@
             {
                 const string waitCommand = "/wait";
 
-                // waitなし？
-                if (!sMessage.StartsWith(waitCommand))
+                try
                 {
-                    SpeechController.Default.Speak(sMessage);
+                    // waitなし？
+                    if (!sMessage.StartsWith(waitCommand))
+                    {
+                        SpeechController.Default.Speak(sMessage);
+                    }
+                    else
+                    {
+                        var values = sMessage.Split(',');
+
+                        // 分割できない？
+                        if (values.Length < 2)
+                        {
+                            // 普通に読上げて終わる
+                            SpeechController.Default.Speak(sMessage);
+                            return;
+                        }
+
+                        var command = values[0].Trim();
+                        var message = values[1].Trim();
+
+                        // 秒数を取り出す
+                        var delayAsText = command.Replace(waitCommand, string.Empty);
+                        int delay = 0;
+                        if (!int.TryParse(delayAsText, out delay))
+                        {
+                            // 普通に読上げて終わる
+                            SpeechController.Default.Speak(sMessage);
+                            return;
+                        }
+
+                        // ディレイをかけて読上げる
+                        SpeechController.Default.SpeakWithDelay(
+                            message,
+                            delay);
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
-                    var values = sMessage.Split(',');
-
-                    // 分割できない？
-                    if (values.Length < 2)
-                    {
-                        // 普通に読上げて終わる
-                        SpeechController.Default.Speak(sMessage);
-                        return;
-                    }
-
-                    var command = values[0].Trim();
-                    var message = values[1].Trim();
-
-                    // 秒数を取り出す
-                    var delayAsText = command.Replace(waitCommand, string.Empty);
-                    int delay = 0;
-                    if (!int.TryParse(delayAsText, out delay))
-                    {
-                        // 普通に読上げて終わる
-                        SpeechController.Default.Speak(sMessage);
-                        return;
-                    }
-
-                    // ディレイをかけて読上げる
-                    SpeechController.Default.SpeakWithDelay(
-                        message,
-                        delay);
+                    ActGlobals.oFormActMain.WriteExceptionLog(
+                        ex,
+                        "TTSYukkuri newTTSで例外が発生しました。");
                 }
             }
         }
