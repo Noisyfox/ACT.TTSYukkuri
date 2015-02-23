a28281c77703821b188808c1fa2a304d3e86a4da
anoyetta <anoyetta@gmail.com>
2015-02-23 12:01:18 +0900

    x64対応

--- ACT.TTSYukkuri/Properties/AssemblyInfo.cs
+++ ACT.TTSYukkuri/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Resources;
 [assembly: ComVisible(false)]
 [assembly: Guid("fc5518a8-181b-43b2-bc12-298484b2919c")]
 
-[assembly: AssemblyVersion("1.6.0.20")]
-[assembly: AssemblyFileVersion("1.6.0.20")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
