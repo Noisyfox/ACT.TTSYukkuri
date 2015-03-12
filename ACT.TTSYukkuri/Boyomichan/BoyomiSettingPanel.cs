6dd961514f6eb854a4e177d1ac9fb01c5cd71f13
anoyetta <anoyetta@gmail.com>
2015-03-12 18:42:22 +0900

    bug fix

--- ACT.TTSYukkuri/Boyomichan/BoyomiSettingPanel.cs
+++ ACT.TTSYukkuri/Boyomichan/BoyomiSettingPanel.cs
@@ -46,29 +46,29 @@
                 this.BoyomiPortNumericUpDown.Value = TTSYukkuriConfig.Default.BoyomiServer.Count > 1 ?
                     int.Parse(TTSYukkuriConfig.Default.BoyomiServer[1]) :
                     BoyomichanSpeechController.BoyomichanServicePort;
-            };
 
-            this.BoyomiServerTextBox.Leave += (s1, e1) =>
-            {
-                if (string.IsNullOrWhiteSpace(this.BoyomiServerTextBox.Text))
+                this.BoyomiServerTextBox.Leave += (s1, e1) =>
                 {
-                    this.BoyomiServerTextBox.Text = "127.0.0.1";
-                }
+                    if (string.IsNullOrWhiteSpace(this.BoyomiServerTextBox.Text))
+                    {
+                        this.BoyomiServerTextBox.Text = "127.0.0.1";
+                    }
 
-                TTSYukkuriConfig.Default.BoyomiServer.Clear();
-                TTSYukkuriConfig.Default.BoyomiServer.Add(this.BoyomiServerTextBox.Text);
-                TTSYukkuriConfig.Default.BoyomiServer.Add(this.BoyomiPortNumericUpDown.Value.ToString());
+                    TTSYukkuriConfig.Default.BoyomiServer.Clear();
+                    TTSYukkuriConfig.Default.BoyomiServer.Add(this.BoyomiServerTextBox.Text);
+                    TTSYukkuriConfig.Default.BoyomiServer.Add(this.BoyomiPortNumericUpDown.Value.ToString());
 
-                TTSYukkuriConfig.Default.Save();
-            };
+                    TTSYukkuriConfig.Default.Save();
+                };
 
-            this.BoyomiPortNumericUpDown.ValueChanged += (s1, e1) =>
-            {
-                TTSYukkuriConfig.Default.BoyomiServer.Clear();
-                TTSYukkuriConfig.Default.BoyomiServer.Add(this.BoyomiServerTextBox.Text);
-                TTSYukkuriConfig.Default.BoyomiServer.Add(this.BoyomiPortNumericUpDown.Value.ToString());
+                this.BoyomiPortNumericUpDown.ValueChanged += (s1, e1) =>
+                {
+                    TTSYukkuriConfig.Default.BoyomiServer.Clear();
+                    TTSYukkuriConfig.Default.BoyomiServer.Add(this.BoyomiServerTextBox.Text);
+                    TTSYukkuriConfig.Default.BoyomiServer.Add(this.BoyomiPortNumericUpDown.Value.ToString());
 
-                TTSYukkuriConfig.Default.Save();
+                    TTSYukkuriConfig.Default.Save();
+                };
             };
         }
     }
