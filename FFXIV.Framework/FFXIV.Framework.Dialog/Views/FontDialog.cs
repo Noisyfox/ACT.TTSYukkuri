68a87df93083cc416e4bc58c2b16cc6341276d0d
anoyetta <anoyetta@gmail.com>
2017-08-23 15:48:56 +0900

    フレームワークをバージョンアップ

---
+++ FFXIV.Framework/FFXIV.Framework.Dialog/Views/FontDialog.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using FFXIV.Framework.Common;
+using FirstFloor.ModernUI.Windows.Controls;
+
+namespace FFXIV.Framework.Dialog.Views
+{
+    public static class FontDialog
+    {
+        private static FontDialogContent content = new FontDialogContent();
+
+        public static FontInfo Font
+        {
+            get => FontDialog.content.FontInfo;
+            set => FontDialog.content.FontInfo = value;
+        }
+
+        public static bool? ShowDialog()
+        {
+            var dialog = new ModernDialog
+            {
+                Title = "Fonts ...",
+                Content = FontDialog.content,
+                MaxWidth = 1280,
+                MaxHeight = 768,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                ShowInTaskbar = true,
+            };
+
+            dialog.Buttons = new Button[] { dialog.OkButton, dialog.CancelButton };
+            dialog.OkButton.Click += FontDialog.content.OKBUtton_Click;
+
+            return dialog.ShowDialog();
+        }
+
+        public static bool? ShowDialog(
+            Window owner)
+        {
+            var starupLocation = owner != null ?
+                WindowStartupLocation.CenterOwner :
+                WindowStartupLocation.CenterScreen;
+
+            var dialog = new ModernDialog
+            {
+                Title = "Fonts ...",
+                Content = FontDialog.content,
+                Owner = owner,
+                MaxWidth = 1280,
+                MaxHeight = 768,
+                WindowStartupLocation = starupLocation,
+                ShowInTaskbar = true,
+            };
+
+            dialog.Buttons = new Button[] { dialog.OkButton, dialog.CancelButton };
+            dialog.OkButton.Click += FontDialog.content.OKBUtton_Click;
+
+            return dialog.ShowDialog();
+        }
+
+        public static bool? ShowDialog(
+            System.Windows.Forms.Form owner)
+        {
+            var starupLocation = owner != null ?
+                WindowStartupLocation.CenterOwner :
+                WindowStartupLocation.CenterScreen;
+
+            var dialog = new ModernDialog
+            {
+                Title = "Fonts ...",
+                Content = FontDialog.content,
+                MaxWidth = 1280,
+                MaxHeight = 768,
+                WindowStartupLocation = starupLocation,
+                ShowInTaskbar = true,
+            };
+
+            if (owner != null)
+            {
+                var helper = new WindowInteropHelper(dialog);
+                helper.Owner = owner.Handle;
+            }
+
+            dialog.Buttons = new Button[] { dialog.OkButton, dialog.CancelButton };
+            dialog.OkButton.Click += FontDialog.content.OKBUtton_Click;
+
+            return dialog.ShowDialog();
+        }
+    }
+}
---
+++ deploy/deploy.ps1
@@ -0,0 +1,24 @@
+﻿'配布対象外のファイルを削除する'
+if (Test-Path deploy.dll) {
+  Remove-Item deploy.dll -Force
+}
+
+if (Test-Path CeVIO.Talk.RemoteService.dll) {
+  Remove-Item CeVIO.Talk.RemoteService.dll -Force
+}
+
+Remove-Item -Force -Recurse -Path .\* -Include *.xml -Exclude *系*,preset-*
+Remove-Item -Force -Recurse -Path .\* -Include *.pdb
+Remove-Item -Force -Recurse -Path .\* -Include "Action icons",locale
+'Done'
+
+'配布ファイルをアーカイブする'
+if (Test-Path deploy.zip) {
+  Remove-Item deploy.zip -Force
+}
+
+$files = Get-ChildItem -Path .\ -Exclude deploy.ps1
+Compress-Archive -CompressionLevel Optimal -Path $files -DestinationPath deploy.zip
+'Done'
+
+Read-Host "終了するには何かキーを教えてください..."
